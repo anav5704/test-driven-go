f6d685ae9dd6d3ce4834d8ae43e8bd2e98f510f8
anav5704 <anavkki@gmail.com>
2024-05-13 00:01:04 +1200

    refactor: add test case for name and empty string

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-    got := Hello("Anav")
-    want := "Hello Anav"
+    t.Run("Say hello to person when name is passed as an argument", func(t *testing.T){
+        got := Hello("Anav")
+        want := "Hello Anav"
 
+        assertCorrectMesage(t, got, want)
+    })
+    t.Run("Say Hello World when empty string is passed as an argument", func(t *testing.T){
+        got := Hello("")
+        want := "Hello World"
+
+        assertCorrectMesage(t, got, want)
+    })
+}
+
+func assertCorrectMesage(t testing.TB, got, want  string){
+    t.Helper()
     if got != want {
         t.Errorf("got %q want %q", got, want)
     }
