f6e65315529d01080d1f913afaf1b149eeacf5f6
anav5704 <anavkki@gmail.com>
2024-05-13 15:59:36 +1200

    refactor: add test case for different languages

--- hello_test.go
+++ hello_test.go
@@ -3,15 +3,30 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-    t.Run("Say hello to person when name is passed as an argument", func(t *testing.T){
-        got := Hello("Anav")
+    t.Run("Say hello to person when name is passed as an argument with no language", func(t *testing.T){
+        got := Hello("", "Anav")
         want := "Hello Anav"
 
         assertCorrectMesage(t, got, want)
     })
-    t.Run("Say Hello World when empty string is passed as an argument", func(t *testing.T){
-        got := Hello("")
+
+    t.Run("Say Hello World when empty strings are passed as arguments", func(t *testing.T){
+        got := Hello("", "")
         want := "Hello World"
+
+        assertCorrectMesage(t, got, want)
+    })
+
+    t.Run("In Spanish", func(t *testing.T){
+        got := Hello("Spanish", "Anav")
+        want:= "Hola Anav"
+
+        assertCorrectMesage(t, got, want)
+    })
+
+    t.Run("In French", func(t *testing.T){
+        got := Hello("French", "Anav")
+        want:= "Bonjour Anav"
 
         assertCorrectMesage(t, got, want)
     })
