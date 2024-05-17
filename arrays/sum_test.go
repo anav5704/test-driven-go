f484c6fb57bdf09c6fd14618e386fde521acebc6
anav5704 <anavkki@gmail.com>
2024-05-18 00:07:47 +1200

    test: add test for sum all function

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,16 +1,27 @@
 package arrays
 
-import "testing"
+import (
+    "reflect"
+    "testing"
+)
 
 func TestSum(t *testing.T){
-    t.Run("Using collection (slice) of 5 numbers", func(t *testing.T) {    
-        numbers := []int{1, 2, 3, 4, 5}
+    numbers := []int{1, 2, 3, 4, 5}
 
-        got := Sum(numbers)
-        want := 15
+    got := Sum(numbers)
+    want := 15
 
-        if got != want {
-            t.Errorf("got %d want %d", got, want) 
-        }
-    })
+    if got != want {
+        t.Errorf("got %d want %d", got, want) 
+    }
 }
+
+func TestSumAll(t *testing.T){
+    got := SumAll([]int{1, 2, 3}, []int{4, 5})   
+    want := []int{6, 9}
+
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got %v want %v", got, want) 
+    }
+}
+
