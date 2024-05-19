8f6826eb97066d46b625a91337aaadc654a510c8
anav5704 <anavkki@gmail.com>
2024-05-19 22:43:32 +1200

    test: add test for area using methods

--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -2,20 +2,34 @@ package structs
 
 import "testing"
 
-func TestPerimeter(t *testing.T){
-    got := Perimeter(10.5, 10.5)
-    want := 42.0
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.5, 10.5}
+	got := Perimeter(rectangle)
+	want := 42.0
 
-    if got != want {
-        t.Errorf("Want %.2f but got %.2f", want, got)
-    }
+	if got != want {
+		t.Errorf("Want %.2f but got %.2f", want, got)
+	}
 }
 
-func TestAraa(t *testing.T){
-    got := Area(10.5, 10.5)
-    want := 110.25
+func TestAraa(t *testing.T) {
+	t.Run("Area of rectanle", func(t *testing.T) {
+		rectangle := Rectangle{10.5, 10.5}
+		got := rectangle.Area()
+		want := 110.25
 
-    if got != want {
-        t.Errorf("Want %.2f but got %.2f", want, got)
-    }
+		if got != want {
+			t.Errorf("Want %.2f but got %.2f", want, got)
+		}
+	})
+
+	t.Run("Area of cirlce", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("Want %g but got %g", want, got)
+		}
+	})
 }
