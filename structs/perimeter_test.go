7741d712a54cdf3d293b353308f80f5c4134599b
Anav Chand <anavkki@gmail.com>
2024-05-21 08:58:49 +1200

    refactor: use interfaces in test cases

--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -3,33 +3,31 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.5, 10.5}
-	got := Perimeter(rectangle)
-	want := 42.0
+    rectangle := Rectangle{10.5, 10.5}
+    got := Perimeter(rectangle)
+    want := 42.0
 
-	if got != want {
-		t.Errorf("Want %.2f but got %.2f", want, got)
-	}
+    if got != want {
+        t.Errorf("Want %.2f but got %.2f", want, got)
+    }
 }
 
-func TestAraa(t *testing.T) {
-	t.Run("Area of rectanle", func(t *testing.T) {
-		rectangle := Rectangle{10.5, 10.5}
-		got := rectangle.Area()
-		want := 110.25
+func TestArea(t *testing.T) {
+    checkArea := func(t testing.TB, shape Shape, want float64){
+        t.Helper()
+        got := shape.Area()
+        if got != want {
+            t.Errorf("Want %g but got %g", want, got)
+        }
+    }
 
-		if got != want {
-			t.Errorf("Want %.2f but got %.2f", want, got)
-		}
-	})
+    t.Run("Area of rectangle", func(t *testing.T) {
+        rectangle := Rectangle{10.5, 10.5}
+        checkArea(t, rectangle, 110.25)
+    })
 
-	t.Run("Area of cirlce", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("Want %g but got %g", want, got)
-		}
-	})
+    t.Run("Area of circle", func(t *testing.T) {
+        circle := Circle{10}
+        checkArea(t, circle, 314.1592653589793)
+    })
 }
