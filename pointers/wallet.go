98d90d57a5882a340d6e4d70240d6df88f1b6193
Anav Chand <anavkki@gmail.com>
2024-06-25 08:20:02 +1200

    test: add case for deposit and withdraw

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,14 +1,19 @@
 package wallet
 
+type BitCoin int
 
 type Wallet struct {
-    balance int
+    balance BitCoin
 }
 
-func (w *Wallet) Deposit (amount int){
+func (w *Wallet) Deposit(amount BitCoin){
     w.balance += amount
 }
 
-func (w *Wallet) Balance () int {
+func (w *Wallet) Balance() BitCoin {
     return w.balance
+}
+
+func (w *Wallet) Withdraw(amount BitCoin) {
+    w.balance -= amount
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,15 +3,32 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T){
-    wallet := Wallet{}
+    t.Run("Deposit bitcoin", func (t * testing.T){
+        wallet := Wallet{}
 
-    wallet.Deposit(10)
+        wallet.Deposit(BitCoin(10))
 
-    got := wallet.Balance()
-    want := 10
+        got := wallet.Balance()
+        want := BitCoin(10)
 
-    if(got != want){
-        t.Errorf("got %d but wanted %d", got, want)
-    }
+        if(got != want){
+            t.Errorf("got %d but wanted %d", got, want)
+        }
+    })
+    
+    t.Run("Withdraw bitcoin", func (t *testing.T){
+        wallet := Wallet{balance: BitCoin(10)}
+
+        wallet.Withdraw(BitCoin(10))
+
+        got := wallet.Balance()
+        want := BitCoin(0)
+
+        if(got != want){
+            t.Errorf("got %d but wanted %d", got, want)
+        }
+
+    })
 }
+
 
