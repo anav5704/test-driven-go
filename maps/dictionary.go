a38842b556ef2613f2e891c94fe110a12246b750
Anav Chand <anavkki@gmail.com>
2024-06-26 10:06:04 +1200

    feat: add to dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -7,14 +7,15 @@ type Dictionary map[string]string
 var ErrNotFound = errors.New("word not found")
 
 func (d Dictionary) Search(query string) (string, error) {
-	 definition, ok := d[query]
+    definition, ok := d[query]
 
-	if !ok {
-		return "", ErrNotFound
-	}
+    if !ok {
+        return "", ErrNotFound
+    }
 
-	return definition, nil
+    return definition, nil
 }
 
-
-
+func (d Dictionary) Add (word, definition string) {
+    d[word] = definition
+}
