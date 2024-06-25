c1e5412ef75b47f4140c821096280bcaf35ad36f
Anav Chand <anavkki@gmail.com>
2024-06-26 09:48:58 +1200

    refactor: make dictionary custom type with methods

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,20 @@
 package dictionary
 
-func search(dictionary map[string]string, query string) string {
-    return dictionary[query]
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("word not found")
+
+func (d Dictionary) Search(query string) (string, error) {
+	 definition, ok := d[query]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
+
+
+
